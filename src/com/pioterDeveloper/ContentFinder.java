5c81354ca47d2718807a39b100b5b09398a71268
96peterharris <55706590+96peterharris@users.noreply.github.com>
2019-12-15 19:31:54 +0100

    All works perfect

--- src/com/pioterDeveloper/ContentFinder.java
+++ src/com/pioterDeveloper/ContentFinder.java
@@ -17,19 +17,19 @@ public class ContentFinder extends Content{
         open.openFIle(fileName);
         webPage = open.getContent();
     }
-   public void findImgae(){
+    public void findImgae(){
 
-       Matcher title_matcher = Pattern.compile("title=\"[a-zA-Z0-9\\s]+").matcher(webPage);
-       Matcher url_matcher = Pattern.compile("(http|https):\\/\\/([a-zA-Z0-9]+.[a-z]+|www.[a-zA-Z0-9]+.[a-z]+)\\/(?:[:a-zA-Z0-9\\s\\/.:_-]*)[.](png|jpg|svg|gif|jpeg|swf)").matcher(webPage);
+        Matcher title_matcher = Pattern.compile("title=\\\"[a-zA-Z0-9\\s-\"]+>").matcher(webPage);
+        Matcher url_matcher = Pattern.compile("(http|https):\\/\\/([a-zA-Z0-9]+.[a-z]+|www.[a-zA-Z0-9]+.[a-z]+)\\/(?:[:a-zA-Z0-9\\s\\/.:_-]*)[.](png|jpg|svg|gif|jpeg|swf)").matcher(webPage);
 
 
-       while(title_matcher.find() && url_matcher.find()) {
-           Content temp = new Content();
-           temp.setTitle(title_matcher.group(0).replaceAll("title=\"", ""));
-           temp.setImg_url(url_matcher.group(0));
-           imageBase.add(temp);
-       }
-   }
+        while(title_matcher.find() && url_matcher.find()) {
+            Content temp = new Content();
+            temp.setTitle(title_matcher.group(0).replaceAll("title=\"", "").replaceAll("\">",""));
+            temp.setImg_url(url_matcher.group(0));
+            imageBase.add(temp);
+        }
+    }
 
     public ArrayList<Content> getImageBase() {
         return imageBase;
--- src/com/pioterDeveloper/Main.java
+++ src/com/pioterDeveloper/Main.java
@@ -4,17 +4,20 @@ package com.pioterDeveloper;
 
 
 import org.apache.commons.io.IOUtils;
+import org.junit.Assert;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.ByteBuffer;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
 import java.util.ArrayList;
 
-
+import static org.junit.Assert.*;
 
 
 public class Main {
@@ -52,9 +55,15 @@ public class Main {
     public static void urlToImage(String url, String fileName) throws IOException {
 
         URL website = new URL(url);
-        ReadableByteChannel rbc = Channels.newChannel(website.openStream());
-        FileOutputStream fos = new FileOutputStream(fileName +".jpg");
-        fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+
+        if(website.openConnection() != null) {
+            ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+            FileOutputStream fos = new FileOutputStream("downloads/" + fileName + ".jpg");
+            fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+        }
+        else{
+            System.out.println("Connection error: " + url);
+        }
 
     }
 
@@ -63,11 +72,52 @@ public class Main {
         try{
 
             for(Content c: content) {
-                urlToImage(c.img_url, c.title);
+                URL tmp = new URL(c.img_url);
+                HttpURLConnection huc = (HttpURLConnection) tmp.openConnection();
+                int responseCode = huc.getResponseCode();
+                //Assert.assertEquals(HttpURLConnection.HTTP_OK, responseCode);
+                if(responseCode == 200){
+                    urlToImage(c.img_url, c.title);
+                }
+                else
+                {
+                    System.out.println("Response code: " + responseCode + " - " + huc.getResponseMessage() + " File: " + "\"" + c.title + "\"");
+                }
             }
         }
         catch(IOException e){
-            System.out.println(e.getMessage());
+           // System.out.println(e.printStackTrace());
+            e.printStackTrace();
         }
     }
+
+   /* public  static void downloader(ArrayList<Content> content){
+        ReadableByteChannel rbc = null;
+        int i = 0;
+        try{
+            for(i = 0; i < content.size(); i++){
+                URL website = new URL(content.get(i).img_url);
+                rbc = Channels.newChannel(website.openStream());
+                FileOutputStream fos = new FileOutputStream(content.get(i).title + ".jpg");
+                fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+            }
+        }
+        catch(IOException e){
+            System.out.println(e.getCause());
+            i++;
+        }
+        finally {
+            try{
+                rbc.close();
+                URL website = new URL(content.get(i).img_url);
+                rbc = Channels.newChannel(website.openStream());
+                FileOutputStream fos = new FileOutputStream(content.get(i).title + ".jpg");
+                fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+
+            }catch(IOException e){
+                System.out.println(e.getCause());
+            }
+        }
+
+    }*/
 }
